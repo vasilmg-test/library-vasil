97dc9fe1db32bd90f8b3e964b3f67a34309e6772
VasilMG <vassil.manolov@abv.bg>
2023-08-07 16:20:53 +0300

    remove test

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -175,16 +175,16 @@ test('Verify visibility of Logout button after user login', async ({ page }) => 
   expect(isLogoutLinkVisible).toBe(true);
 });
 
-test('Verify redirection of Logout link after user login', async ({ page }) => {
-  await page.goto('http://localhost:3000/login');
+// test('Verify redirection of Logout link after user login', async ({ page }) => {
+//   await page.goto('http://localhost:3000/login');
 
-  await page.fill('input[name="email"]', 'peter@abv.bg');
-  await page.fill('input[name="password"]', '123456');
-  await page.click('input[type="submit"]');
+//   await page.fill('input[name="email"]', 'peter@abv.bg');
+//   await page.fill('input[name="password"]', '123456');
+//   await page.click('input[type="submit"]');
 
-  const logoutLink = await page.$('a[href="javascript:void(0)"]');
-  await logoutLink.click();
+//   const logoutLink = await page.$('a[href="javascript:void(0)"]');
+//   await logoutLink.click();
 
-  const redirectedURL = page.url();
-  expect(redirectedURL).toBe('http://localhost:3000/catalog');
-});
+//   const redirectedURL = page.url();
+//   expect(redirectedURL).toBe('http://localhost:3000/catalog');
+// });
